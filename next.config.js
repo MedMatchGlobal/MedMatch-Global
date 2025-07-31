ca3168cbfba12050dba3d95f1cf713456a4b00bd
Sergio Pasqualini <sergio@gesconsultancy.eu>
2025-07-31 14:28:16 +0100

    Fix: enforce App Router via appDir in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true, // 👈 Tells Next to use the App Router
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
