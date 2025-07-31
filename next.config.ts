ff3e88a96697dadcd53dc534024146cc19fd037e
Sergio Pasqualini <sergio@gesconsultancy.eu>
2025-07-31 13:56:52 +0100

    Fix: enable appDir for Next.js App Router

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
-import path from 'path';
-import { NextConfig } from 'next';
+import path from "path";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   },
 };
