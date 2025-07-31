d2d37af085f739a1c8f3bd23c89713054167808b
Sergio Pasqualini <sergio@gesconsultancy.eu>
2025-07-31 14:36:55 +0100

    Fix: rename config and enable appDir

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    appDir: true, // 👈 Tells Next to use the App Router
-  },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
+    appDir: true, // Important for App Router support
   },
 };
 
